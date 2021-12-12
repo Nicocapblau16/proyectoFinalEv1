86570bbe1306c74beb3d6cf08f920cefc2c2c68e
Nicolau Guardiola <nico.guardiolat@gmail.com>
2021-12-12 17:27:21 +0100

    2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,14 +12,48 @@ public class PlayerController : MonoBehaviour
     private float limZ = 200f;
 
     private  float speed = 20f;
+    //private float horizontalInput;
+    //private float verticalInput;
+
+    public AudioClip shootClip;
+
+    private AudioSource cameraAudioSource;
+    private AudioSource playerAudioSource;
 
     public GameObject projectilePrefab;
+    public GameObject recoletable;
+    public GameObject obstacle;
+
+    private float spawnRate = 5f;
+    private float spawnMargin = 5f;
+
+    private Vector3 randomPos;
+
+    private int score;
+
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //
+        // posicion inicial
         transform.position = startPos;
+
+        //marcador a 0
+        score = 0;
+
+        //spawn de recolectables
+        for (float coinInstances = 10f; coinInstances >=0; coinInstances -= 1f)
+        {
+            randomPos = RandomPosition();
+            Instantiate(recoletable, randomPos, recoletable.transform.rotation);
+        }
+
+        //inicio corrutina
+
+
+        cameraAudioSource = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+        playerAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,6 +61,9 @@ public class PlayerController : MonoBehaviour
     {
         //movimiento constante
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+       // horizontalInput = Input.GetAxis("Horizontal");
+        //verticalInput = Input.GetAxis("Vertical");
 
         //limites
         if (transform.position.x <= -limX)
@@ -65,7 +102,9 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.RightControl))
         {
             //disparo
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation = transform.rotation);
+
+            playerAudioSource.PlayOneShot(shootClip, 1);
         }
     }
 
@@ -75,5 +114,24 @@ public class PlayerController : MonoBehaviour
         {
             transform.rotation *= Quaternion.Euler(rotation);
         }
+    }
+
+
+    // Genera una posicion aleatoria 
+    public Vector3 RandomPosition()
+    {
+        return new Vector3(Random.Range(-limX + spawnMargin, limX - spawnMargin), Random.Range(limLowY + spawnMargin, limY - spawnMargin), Random.Range(-limZ + spawnMargin, limZ - spawnMargin));
+    }
+
+    private IEnumerator SpawnObstacle()
+    {
+        while (!gameOver)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            randomPos = RandomPosition();
+            Instantiate(obstacle, randomPos, recoletable.transform.rotation);
+
+        }
+
     }
 }
